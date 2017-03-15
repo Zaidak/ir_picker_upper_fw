0050d24ad319950c63f2089b7ae64415d39e9ea8
Zaid A.K <z_alk@live.concordia.ca>
2017-03-15 12:55:02 -0400

    Updated firmware connection logic and feadback
    Connection is now initialized by the server with +
    board LED is used for feadback to show when waiting for server,
    connected, ready to record, and done recording.

--- irpu/irpu.ino
+++ irpu/irpu.ino
@@ -1,10 +1,17 @@
+/*
+2017-03-15 - update: 
+  - revise logic: 
+    * On startup the arduino is in waiting mode, builtin LED on pin 13 is to keep blinking, until a server connect request is received. 
+  - improved feadback
+    * When server is connected the LED will blink twice then ACK connection
+    * when ready to record a signal, the LED is ON, when done, the LED is OFF.
+*/
 
 // *********************** Communication Chars
 //                     Arduino     Server
-#define STAR 42       // '*'
+#define S_STARTED 43  // '+'    <- Server is started and trying to connect and remove the arduino from the inital waiting state
 #define I_STARTED 44  // ','   -> Inform Server Arduino is ready
-#define S_STARTED 43  // '+'    <- Inform Arduino Server heared the I_STARED and is listening
-#define READY 45      // '-'    <- Prepare Arduino to record a signal
+#define ARM_TO_RECORD 45      // '-'    <- Prepare Arduino to record a signal
 #define REPEAT 46     // '.'    <- Repeat the last signal recorded
 #define FREQ 47       // '/'    <- Set the pulsing freq with the next received char
 #define ACK 50
@@ -19,7 +26,7 @@
 #define diPin 8// 8 for shield // 13 for breadboard prototype // A0 // IR sensor input 
 #define LEDPin 9 // 9 for shield // 4 for breadboard prototype
 #define pulsePin 11 // 11 for shield, //12 for breadboard prototype
-//#define boardLED 13
+#define boardLED 13
 
 bool dIn[takes];
 unsigned char encoded[encodedSize];
@@ -37,14 +44,36 @@ void setup() {
   pinMode(diPin, INPUT);
   pinMode(LEDPin, OUTPUT);
   pinMode(pulsePin, OUTPUT);
+  pinMode(boardLED, OUTPUT);
   digitalWrite(LEDPin, LOW);
   digitalWrite(pulsePin, LOW);
-    
   Serial.begin(9600); 
-  Serial.println(I_STARTED);    // Allow servers to know arduino just started..
-  while(1){                     // Wait for server's ACK (S_STARTED)
-    if(Serial.read()==S_STARTED){ connConfirmed = true; Serial.println(ACK);break;}
+
+  int blinktimer = 0;
+  bool board_led_status = false;
+  while(1){ // Waiting for server request to connect. did we get S_STARTED ?
+    if(Serial.read()==S_STARTED){
+      digitalWrite(boardLED, LOW);      delay(150);
+      digitalWrite(boardLED,HIGH);      delay (200);
+      digitalWrite(boardLED, LOW);      delay(100);
+      digitalWrite(boardLED,HIGH);      delay (200);
+      digitalWrite(boardLED, LOW);
+      
+      Serial.println(ACK); 
+      break;
+    }
+    blinktimer++;
+    if(blinktimer> 20000){
+      board_led_status = board_led_status ? false: true;
+      blinktimer = 0;
+    }
+    digitalWrite(boardLED, board_led_status? HIGH : LOW);
   }
+    
+  Serial.println(I_STARTED);    // Allow servers to know arduino just started..
+ /* while(1){                     // Wait for server's ACK (S_STARTED)
+    if(Serial.read()==S_STARTED){ connConfirmed = true; Serial.println(ACK);break;}
+  }*/
 
   tone (pulsePin, 38000); // send to port 12 a pulse with frequency 38 KHz
   for(int i=0;i<encodedSize; i++) encoded[i] = -1; // init array
@@ -55,7 +84,8 @@ void loop() {
   {
     in = Serial.read();
     switch (in){
-    case READY:// ready to sense an IR signal
+    case ARM_TO_RECORD:// ready to sense an IR signal
+    digitalWrite(boardLED, HIGH);
       while(1){
         sensorValue = digitalRead(diPin); 
         if ((sensorValue ==1) /*&& (done == false)*/)
@@ -101,7 +131,8 @@ void loop() {
           Serial.println("d");
           break;
         }
-      }
+      } // DONE RECORDING
+      digitalWrite(boardLED,LOW);
       break;
     case REPEAT:
       //while(1){
@@ -129,3 +160,5 @@ void loop() {
     }
   }
 }
+
+
